a74005ab8f5e052e6e4a92968c12015aa9ca149b
Kevin Luciano <luciano19940@hotmail.com>
2025-12-03 08:46:45 -0600

    AI Project

---
+++ main.py
@@ -0,0 +1,12 @@
+# pip install -qU langchain "langchain[anthropic]"
+from langchain.agents import create_agent
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="openai:gpt-4o-mini",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
